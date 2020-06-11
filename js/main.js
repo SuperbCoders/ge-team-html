f82217e14e81eb680f50d3796fa14642bf127fc6
Лев Ованоглян <levkino@yandex.ru>
2020-06-11 10:02:16 +0300

    Добавил страницу Team.

--- js/main.js
+++ js/main.js
@@ -19,7 +19,22 @@ $('a[href*="#"]').on('click', function(e) {
 
 // Загрузка страницы
 $(document).ready(function() {
-  $('.clients__big-title-first').marquee({ duration: 8000, gap: 50, duplicated: true });
-  setTimeout(() => $('.clients__big-title-second').marquee({ duration: 8000, gap: 50, duplicated: true }), 2000);
-  setTimeout(() => $('.clients__big-title-third').marquee({ duration: 8000, gap: 50, duplicated: true }), 4000);
+  const marquee1 = $('.clients__big-title-first');
+  const marquee2 = $('.clients__big-title-second');
+  const marquee3 = $('.clients__big-title-third');
+  if (marquee1.length) {
+    marquee1.marquee({ duration: 8000, gap: 50, duplicated: true });
+  }
+  if (marquee2.length) {
+    setTimeout(() => {
+      marquee2.css('display', 'block');
+      marquee2.marquee({ duration: 8000, gap: 50, duplicated: true });
+    }, 2000);
+  }
+  if (marquee3.length) {
+    setTimeout(() => {
+      marquee3.css('display', 'block');
+      marquee3.marquee({ duration: 8000, gap: 50, duplicated: true });
+    }, 4000);
+  }
 });
