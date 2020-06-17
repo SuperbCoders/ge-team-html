a870a301986a61d510512e7dddbd52a3aea1acb9
logeeka <logeekal@gmail.com>
2020-06-17 14:37:32 +0300

    checkboxes fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 let expanded = false;
 const menu = document.querySelector('.header__menu');
 const burger = document.querySelector('.burger');
+
 burger.addEventListener('click', function() {
   this.classList.toggle('active');
   menu.classList.toggle('active');
-  document.body.classList.toggle('lock');  
+  document.body.classList.toggle('lock');
+  expanded = true;
+  toggleCheckboxes();
 });
 
 function scrollToHash(selector) {
